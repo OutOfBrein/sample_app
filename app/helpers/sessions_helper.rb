d64e48272505e2b53268de542b3656706ac07a21
outofbrein <outofbrein@gmail.com>
2011-10-24 15:32:46 +0300

    Excercise-9-6-2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,8 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    #cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:user_id] = user.id
     self.current_user = user
   end
   
@@ -10,7 +11,8 @@ module SessionsHelper
   end
   
   def current_user
-    @current_user ||= user_from_remember_token
+    #@current_user ||= user_from_remember_token
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
   def signed_in?
@@ -18,18 +20,19 @@ module SessionsHelper
   end
   
   def sign_out
-    cookies.delete(:remember_token)
+    #cookies.delete(:remember_token)
+    session[:user_id] = nil
     self.current_user = nil
   end
 
   private
 
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
+    #def user_from_remember_token
+    #  User.authenticate_with_salt(*remember_token)
+    #end
 
-    def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
-    end
+    #def remember_token
+    #  cookies.signed[:remember_token] || [nil, nil]
+    #end
   
 end
